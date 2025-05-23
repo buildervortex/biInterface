af68710ce52140f2d131fe1f50848e401c6e952c
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-05-23 20:08:05 +0530

    Correct Customer Registration Stats

--- view/figures/CylinderCountFigure.py
+++ view/figures/CylinderCountFigure.py
@@ -1,3 +1,35 @@
+# import plotly.graph_objects as go
+# from repository.biDashboardRepository import BiDashboardRepository
+
+# def CylinderCountFigure(repository: BiDashboardRepository):
+#     data = repository.getCylinderCountFromUserToGiveUs()
+#     counter = data
+    
+
+#     # Correct data for Pie chart
+#     labels = ['Cylinder Count From User To Give Us']
+#     values = [ counter]
+
+#     # Pull each slice slightly out
+#     pull = [0.1, 0.1]
+
+#     fig = go.Figure(data=[go.Pie(
+#         labels=labels,
+#         values=values,
+#         pull=pull,
+#         marker=dict(line=dict(color='white', width=8)),
+#         textinfo='label+value',
+#         hole=0  # Set to 0.4 if you want a donut chart
+#     )])
+
+#     fig.update_layout(
+#         height=600,
+#         width=600
+#     )
+
+#     return fig
+
+
 import plotly.graph_objects as go
 from repository.biDashboardRepository import BiDashboardRepository
 
@@ -5,13 +37,11 @@ def CylinderCountFigure(repository: BiDashboardRepository):
     data = repository.getCylinderCountFromUserToGiveUs()
     counter = data
     
-
-    # Correct data for Pie chart
     labels = ['Cylinder Count From User To Give Us']
-    values = [ counter]
+    values = [counter]
 
-    # Pull each slice slightly out
-    pull = [0.1, 0.1]
+    # Pull each slice slightly out (only one slice here, so one value)
+    pull = [0.1]
 
     fig = go.Figure(data=[go.Pie(
         labels=labels,
@@ -19,7 +49,7 @@ def CylinderCountFigure(repository: BiDashboardRepository):
         pull=pull,
         marker=dict(line=dict(color='white', width=8)),
         textinfo='label+value',
-        hole=0  # Set to 0.4 if you want a donut chart
+        hole=0  # 0 for pie chart, >0 for donut chart
     )])
 
     fig.update_layout(
@@ -27,4 +57,8 @@ def CylinderCountFigure(repository: BiDashboardRepository):
         width=600
     )
 
+    # To display the figure immediately (if running interactively)
+    # fig.show()
+
+    # To return the figure object for later use
     return fig
