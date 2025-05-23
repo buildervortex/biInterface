6114aa603828676b97df122ba0ef698d5413bda9
galahirudilhara <galahirudilhara@gmail.com>
2025-05-23 14:34:30 +0530

    add the get loan finished unfinished count method to the repository

--- repository/biDashboardRepository.py
+++ repository/biDashboardRepository.py
@@ -25,7 +25,7 @@ class BiDashboardRepository:
             data.append((name, empty_count, filled_count))
 
         return data
-    
+
     def getGasIncome(self, inventory_id: int) -> list[tuple[str, int, int]]:
         query: str = f"select ct.display_name,cs.cylinder_count, (cs.cylinder_count*ct.filled_gas_cylinder_exchange_price) as selling_income, (cs.cylinder_count*ct.cylinder_exchange_profit) as selling_profit  from encompass as cs join cylinder_type as ct on cs.cylinder_type_id=ct.id;"
         result = self.db.exec(query=query)
@@ -40,8 +40,22 @@ class BiDashboardRepository:
         for row in rows:
             name = row.display_name
             selling_income = int(row.selling_income)
-       
 
             data.append((name, selling_income))
 
         return data
+
+    def getLoanFinishedUnfinishedCount(self, previous_date: str) -> tuple[int, int]:
+        query: str = f"select count(payment_id) as count_t,finished from loaned_payment where payment_id in (select id from payment where date_time > {previous_date} ) group by finished;"
+
+        result = self.db.exec(query=query)
+
+        if result == None:
+            return (0, 0)
+
+        rows = result.fetchall()
+
+        unfinished_count = int(rows[0].count_t)
+        finished_count = int(rows[1].count_t)
+
+        return (unfinished_count, finished_count)
---
+++ test.py
@@ -0,0 +1,9 @@
+from database.db import Database
+from repository.biDashboardRepository import BiDashboardRepository
+
+db: Database = Database()
+db.connect()
+
+repository = BiDashboardRepository(db=db)
+
+print(repository.getLoanFinishedUnfinishedCount("2024-04-05"))
